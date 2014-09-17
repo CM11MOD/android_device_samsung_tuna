9d4102ac0927fe72f348c45ccef31b7acde6e47b
Ziyann <jaraidaniel@gmail.com>
2014-09-17 16:08:58 +0200

    tuna: recovery: protect important partitions
    Formatting /factory, /boot, /sbl or /xloader is pointless and dangerous.
    
    Change-Id: I32980c57a71e6f7e7bed761379ce157a035ffaf7

--- device.mk
+++ device.mk
@@ -214,6 +214,10 @@ PRODUCT_PACKAGES += \
         fibmap.f2fs \
         f2fstat
 
+# Recovery
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.cwm.forbid_format=/factory,/boot,/sbl,/xloader
+
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
 $(call inherit-product-if-exists, vendor/nxp/pn544/nxp-pn544-fw-vendor.mk)
